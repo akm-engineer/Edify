d210741ca3d48656dfb0824c6e7607b51f5054eb
Mac <mac@Macs-MacBook-Air.local>
2024-11-27 11:01:06 -0800

    fix issue

--- Redux-task-manager-main/src/App.jsx
+++ Redux-task-manager-main/src/App.jsx
@@ -1,15 +1,17 @@
-import { Provider } from "react-redux";
-import "./App.css";
-import store from "./redux/store";
+import { Provider } from 'react-redux';
+import './App.css';
+import store from './redux/store';
 
-import Task from "./components/Task";
+import Task from './components/Task';
 
 function App() {
-  return (
-    <Provider store={store}>
-      <Task />
-    </Provider>
-  );
+	return (
+		<>
+			<Provider store={store}>
+				<Task />
+			</Provider>
+		</>
+	);
 }
 
 export default App;
